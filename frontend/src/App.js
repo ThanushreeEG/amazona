98c1c99f236b7e6b99f3b1bceeae66921f31ba6d
ThanushreeEG <eg.thanushree@gmail.com>
2024-08-02 20:28:50 -0500

    video5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div className="product" key={products.slug}>
+              <a href={`/products/${products.slug}`}>
+                <img src={products.image} alt={products.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${products.slug}`}>
+                  <p>{products.name}</p>
+                </a>
+                <p>
+                  <strong>${products.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
